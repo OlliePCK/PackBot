582a4b38e0205b83d29c14e8db5f06e4e42d030c
OlliePCK <ollieker3108@gmail.com>
2024-06-26 22:48:20 +1000

    error handling

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 const eventFiles = fs.readdirSync('./events/client').filter(file => file.endsWith('.js'));
 const eventFunctions = fs.readdirSync('./events/event-functions').filter(file => file.endsWith('.js'));
 
-  
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// Set a new item in the Collection
@@ -68,219 +68,291 @@ for (const file of eventFunctions) {
 
 client.distube
 	.on('playSong', (queue, song) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.play} | Now playing: ${song.name}`)
-			.setURL(`${song.url}`)
-			.addFields(
-				{ name: 'Duration', value: `\`${song.formattedDuration}\``, inline: true },
-				{ name: 'Requested by', value: `${song.user}`, inline: true },
-				{ name: 'Volume', value: `\`${queue.volume}%\``, inline: true },
-				{ name: 'Filter', value: `\`${queue.filters.names.join(', ') || 'Off'}\``, inline: true },
-				{ name: 'Loop', value: `${queue.repeatMode ? queue.repeatMode === 2 ? `${client.emotes.repeat} All Queue` : `${client.emotes.repeat} This Song` : 'Off'}`, inline: true },
-				{ name: 'Autoplay', value: `${queue.autoplay ? `${client.emotes.autoplay} On` : 'Off'}`, inline: true },
-			)
-			.setImage(`${song.thumbnail}`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.play} | Now playing: ${song.name}`)
+				.setURL(`${song.url}`)
+				.addFields(
+					{ name: 'Duration', value: `\`${song.formattedDuration}\``, inline: true },
+					{ name: 'Requested by', value: `${song.user}`, inline: true },
+					{ name: 'Volume', value: `\`${queue.volume}%\``, inline: true },
+					{ name: 'Filter', value: `\`${queue.filters.names.join(', ') || 'Off'}\``, inline: true },
+					{ name: 'Loop', value: `${queue.repeatMode ? queue.repeatMode === 2 ? `${client.emotes.repeat} All Queue` : `${client.emotes.repeat} This Song` : 'Off'}`, inline: true },
+					{ name: 'Autoplay', value: `${queue.autoplay ? `${client.emotes.autoplay} On` : 'Off'}`, inline: true },
+				)
+				.setImage(`${song.thumbnail}`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('pause', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.pause} | Music paused!`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.pause} | Music paused!`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('stop', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.stop} | Music stopped!`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.stop} | Music stopped!`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('skip', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.skip} | Skipped: ${queue.previousSongs[0].name}`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.skip} | Skipped: ${queue.previousSongs[0].name}`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('repeatModeChange', (queue, repeatMode) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${repeatMode === 2 ? `${client.emotes.repeat} All Queue` : repeatMode === 1 ? `${client.emotes.repeat} This Song` : 'Off'} | Loop mode changed!`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${repeatMode === 2 ? `${client.emotes.repeat} All Queue` : repeatMode === 1 ? `${client.emotes.repeat} This Song` : 'Off'} | Loop mode changed!`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('shuffle', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.shuffle} | Queue shuffled!`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.shuffle} | Queue shuffled!`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('filterAdd', (queue, filter) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.filter} | Added Filter: \`${filter}\``)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.filter} | Added Filter: \`${filter}\``)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('filterRemove', (queue, filter) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.filter} | Removed Filter: \`${filter}\``)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.filter} | Removed Filter: \`${filter}\``)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('volumeChange', (queue, volume) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.volume} | Volume changed: \`${volume}%\``)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.volume} | Volume changed: \`${volume}%\``)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('autoplayOn', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.autoplay} | Autoplay enabled!`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.autoplay} | Autoplay enabled!`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('autoplayOff', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.autoplay} | Autoplay disabled!`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.autoplay} | Autoplay disabled!`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('volumeNaturallyChanged', (queue, volume) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.volume} | Volume changed to: \`${volume}%\``)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.volume} | Volume changed to: \`${volume}%\``)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('searchResult', (message, result) => {
-		let i = 0;
-		const embed = new EmbedBuilder()
-			.setTitle(`🔎 | Results for: \`${result.query}\``)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		result.items.forEach(song => {
-			i++;
-			if (i > 5) return;
-			embed.addField(`${i}. ${song.name}`, `[Link](${song.url}) - \`${song.formattedDuration}\``);
-		});
-		message.channel.send({ embeds: [embed] });
+		try {
+			let i = 0;
+			const embed = new EmbedBuilder()
+				.setTitle(`🔎 | Results for: \`${result.query}\``)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			result.items.forEach(song => {
+				i++;
+				if (i > 5) return;
+				embed.addField(`${i}. ${song.name}`, `[Link](${song.url}) - \`${song.formattedDuration}\``);
+			});
+			message.channel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('searchCancel', message => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.error} | Search Cancelled`)
-			.setDescription('The search has been cancelled. Please try again.')
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		message.channel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.error} | Search Cancelled`)
+				.setDescription('The search has been cancelled. Please try again.')
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			message.channel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('error', (channel, error) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.error} | Error occurred!`)
-			.setDescription(`An error occurred while executing the command: ${error}`)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		channel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.error} | Error occurred!`)
+				.setDescription(`An error occurred while executing the command: ${error}`)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			channel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('finish', queue => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.success} | Music finished!`)
-			.setDescription('Thank you for using The Pack music bot.')
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.success} | Music finished!`)
+				.setDescription('Thank you for using The Pack music bot.')
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 	.on('addList', (queue, playlist) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.success} | Playlist added: ${playlist.name}`)
-			.setURL(`${playlist.url}`)
-			.addFields(
-				{ name: 'Songs', value: `\`${playlist.songs.length}\``, inline: true },
-				{ name: 'Requested by', value: `${playlist.user}`, inline: true },
-				{ name: 'Duration', value: `\`${playlist.formattedDuration}\``, inline: true },
-			)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		if (playlist.thumbnail) {
-			embed.setImage(`${playlist.thumbnail}`);
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.success} | Playlist added: ${playlist.name}`)
+				.setURL(`${playlist.url}`)
+				.addFields(
+					{ name: 'Songs', value: `\`${playlist.songs.length}\``, inline: true },
+					{ name: 'Requested by', value: `${playlist.user}`, inline: true },
+					{ name: 'Duration', value: `\`${playlist.formattedDuration}\``, inline: true },
+				)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			if (playlist.thumbnail) {
+				embed.setImage(`${playlist.thumbnail}`);
+			}
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
 		}
-		queue.textChannel.send({ embeds: [embed] });
 	})
 	.on('addSong', (queue, song) => {
-		const embed = new EmbedBuilder()
-			.setTitle(`${client.emotes.success} | Song added: ${song.name}`)
-			.setURL(`${song.url}`)
-			.addFields(
-				{ name: 'Duration', value: `\`${song.formattedDuration}\``, inline: true },
-				{ name: 'Requested by', value: `${song.user}`, inline: true },
-				{ name: 'Position in queue', value: `${queue.songs.length}`, inline: true },
-			)
-			.setThumbnail(song.thumbnail)
-			.setFooter({
-				text: 'The Pack',
-				iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
-			})
-			.setColor('#ff006a');
-		queue.textChannel.send({ embeds: [embed] });
+		try {
+			const embed = new EmbedBuilder()
+				.setTitle(`${client.emotes.success} | Song added: ${song.name}`)
+				.setURL(`${song.url}`)
+				.addFields(
+					{ name: 'Duration', value: `\`${song.formattedDuration}\``, inline: true },
+					{ name: 'Requested by', value: `${song.user}`, inline: true },
+					{ name: 'Position in queue', value: `${queue.songs.length}`, inline: true },
+				)
+				.setThumbnail(song.thumbnail)
+				.setFooter({
+					text: 'The Pack',
+					iconURL: 'https://i.imgur.com/5RpRCEY.jpeg'
+				})
+				.setColor('#ff006a');
+			queue.textChannel.send({ embeds: [embed] });
+		} catch (error) {
+			console.error(error);
+		}
 	})
 
 client.login(process.env.TOKEN);
